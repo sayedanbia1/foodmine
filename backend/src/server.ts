07bb95a997faafa8f6f8630db645398ce1f03153
sayedanbia1 <sayedanbia5555@gamil.com>
2023-11-08 10:37:29 +0430

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,23 @@
+
+import dotenv from 'dotenv';
+dotenv.config();
+import express from "express";
+import cors from "cors";
+
+import userRouter from './routers/user.router';
+import { dbConnect } from './configs/database.config';
+import foodRouter from './routers/food.router';
+dbConnect();
+const app = express();
+app.use(express.json());
+app.use(cors({
+    credentials:true,
+    origin:["http://localhost:4200"]
+}));
+app.use("/api/foods", foodRouter);
+app.use("/api/users", userRouter);
+
+const port = 5000;
+app.listen(port, () => {
+    console.log("Website served on http://localhost:" + port);
+})
